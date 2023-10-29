f20f0176301b066b4553efd2c17990a3f38e9e1d
Beatriz Barrera <148925353+bettybbaa@users.noreply.github.com>
2023-10-28 20:50:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// Agarra elemento (HTML)por su ID
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span class="typewriter">Desarrollo sitios web y escribo artículos sobre programación.</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
